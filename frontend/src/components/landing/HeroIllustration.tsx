542630257062f730681f88ef8bf3bdadd605d651
osmanmrtacar <osmanmrtacar@yandex.com>
2026-01-18 00:45:29 -0600

    fix icons

--- frontend/src/components/landing/HeroIllustration.tsx
+++ frontend/src/components/landing/HeroIllustration.tsx
@@ -25,8 +25,10 @@ export function HeroIllustration() {
             <animate attributeName="stroke-dashoffset" from="10" to="0" dur="1s" repeatCount="indefinite" />
           </path>
           <circle cx="480" cy="120" r="35" fill="white" stroke="url(#gradient1)" strokeWidth="3" />
-          {/* TikTok icon simplified */}
-          <path d="M475 110 L475 130 M485 110 L485 125 M480 125 Q485 128 490 125" stroke="url(#gradient1)" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+          {/* TikTok icon */}
+          <g transform="translate(465, 105) scale(0.12)">
+            <path d="M224,72a48.05,48.05,0,0,1-48-48,8,8,0,0,0-8-8H128a8,8,0,0,0-8,8V156a20,20,0,1,1-28.57-18.08A8,8,0,0,0,96,130.69V88a8,8,0,0,0-9.4-7.88C50.91,86.48,24,119.1,24,156a76,76,0,0,0,152,0V116.29A103.25,103.25,0,0,0,224,128a8,8,0,0,0,8-8V80A8,8,0,0,0,224,72Zm-8,39.64a87.19,87.19,0,0,1-43.33-16.15A8,8,0,0,0,160,102v54a60,60,0,0,1-120,0c0-25.9,16.64-49.13,40-57.6v27.67A36,36,0,1,0,136,156V32h24.5A64.14,64.14,0,0,0,216,87.5Z" fill="url(#gradient1)" />
+          </g>
         </g>
 
         {/* Instagram - Right */}
@@ -47,9 +49,12 @@ export function HeroIllustration() {
             <animate attributeName="stroke-dashoffset" from="10" to="0" dur="1s" repeatCount="indefinite" />
           </path>
           <circle cx="480" cy="330" r="35" fill="white" stroke="url(#gradient1)" strokeWidth="3" />
-          {/* YouTube icon simplified */}
-          <path d="M 500 330 L 500 330 Q 502 325 495 325 L 465 325 Q 458 325 460 330 L 460 330 Q 458 335 465 335 L 495 335 Q 502 335 500 330 Z" fill="url(#gradient1)" />
-          <path d="M 475 327 L 485 330 L 475 333 Z" fill="white" />
+          {/* YouTube icon */}
+          <g transform="translate(465, 315) scale(0.12)">
+            <path d="M164.44,121.34l-48-32A8,8,0,0,0,104,96v64a8,8,0,0,0,12.44,6.66l48-32a8,8,0,0,0,0-13.32ZM120,145.05V111l25.58,17ZM234.33,69.52a24,24,0,0,0-14.49-16.4C185.56,39.88,131,40,128,40s-57.56-.12-91.84,13.12a24,24,0,0,0-14.49,16.4C19.08,79.5,16,97.74,16,128s3.08,48.5,5.67,58.48a24,24,0,0,0,14.49,16.41C69,215.56,120.4,216,127.34,216h1.32c6.94,0,58.37-.44,91.18-13.11a24,24,0,0,0,14.49-16.41c2.59-10,5.67-28.22,5.67-58.48S236.92,79.5,234.33,69.52Zm-15.49,113a8,8,0,0,1-4.77,5.49c-31.65,12.22-85.48,12-86,12H128c-.54,0-54.33.2-86-12a8,8,0,0,1-4.77-5.49C34.8,173.39,32,156.57,32,128s2.8-45.39,5.16-54.47A8,8,0,0,1,41.93,68c30.52-11.79,81.66-12,85.85-12h.27c.54,0,54.38-.18,86,12a8,8,0,0,1,4.77,5.49C221.2,82.61,224,99.43,224,128S221.2,173.39,218.84,182.47Z" fill="url(#gradient1)" />
+          </g>
+          {/* Coming Soon label */}
+          <text x="480" y="380" fontSize="10" fill="url(#gradient1)" textAnchor="middle" fontFamily="Arial, sans-serif" fontWeight="500">Coming Soon</text>
         </g>
 
         {/* Twitter/X - Top Left */}
@@ -58,8 +63,10 @@ export function HeroIllustration() {
             <animate attributeName="stroke-dashoffset" from="10" to="0" dur="1s" repeatCount="indefinite" />
           </path>
           <circle cx="120" cy="120" r="35" fill="white" stroke="url(#gradient2)" strokeWidth="3" />
-          {/* X icon simplified */}
-          <path d="M 110 110 L 130 130 M 130 110 L 110 130" stroke="url(#gradient2)" strokeWidth="2.5" strokeLinecap="round" />
+          {/* X icon */}
+          <g transform="translate(105, 105) scale(0.12)">
+            <path d="M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z" fill="url(#gradient2)" />
+          </g>
         </g>
 
         {/* LinkedIn - Left */}
@@ -69,9 +76,10 @@ export function HeroIllustration() {
           </path>
           <circle cx="100" cy="240" r="35" fill="white" stroke="url(#gradient1)" strokeWidth="3" />
           {/* LinkedIn icon simplified */}
-          <rect x="90" y="235" width="5" height="15" rx="1" fill="url(#gradient1)" />
-          <rect x="100" y="230" width="5" height="20" rx="1" fill="url(#gradient1)" />
-          <rect x="110" y="235" width="5" height="15" rx="1" fill="url(#gradient1)" />
+          <rect x="88" y="228" width="24" height="24" rx="3" fill="url(#gradient1)" />
+          <text x="100" y="244" fontSize="14" fontWeight="bold" fill="white" textAnchor="middle" fontFamily="Arial, sans-serif">in</text>
+          {/* Coming Soon label */}
+          <text x="100" y="290" fontSize="10" fill="url(#gradient1)" textAnchor="middle" fontFamily="Arial, sans-serif" fontWeight="500">Coming Soon</text>
         </g>
 
         {/* Gradients */}
