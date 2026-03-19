26d666cbd5d4dc5553b6394eca0168c70f1c8726
osmanmrtacar <osmanmrtacar@yandex.com>
2026-03-19 00:17:51 -0500

    fix overflow

--- frontend/src/components/post/PostForm.tsx
+++ frontend/src/components/post/PostForm.tsx
@@ -669,10 +669,10 @@ const PostForm = ({ onPostCreated }: PostFormProps) => {
                               {option.description && <span className="text-xs text-gray-400 ml-1">- {option.description}</span>}
                             </button>
                             {isDisabled && (
-                              <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 z-30 hidden group-hover:block w-56">
-                                <div className="bg-gray-900 text-white text-xs rounded-lg px-3 py-2 shadow-lg">
+                              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 z-30 hidden group-hover:block w-56 pointer-events-none">
+                                <div className="bg-gray-900 text-white text-xs rounded-lg px-3 py-2 shadow-lg text-center">
                                   Branded content visibility cannot be set to private.
-                                  <div className="absolute right-full top-1/2 -translate-y-1/2 border-4 border-transparent border-r-gray-900" />
+                                  <div className="absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-gray-900" />
                                 </div>
                               </div>
                             )}
